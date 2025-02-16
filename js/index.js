1c9ce60615a4a83b69f6d909af7f1fca8bae26f5
LuongPhuongHien <bosshieng55@gmail.com>
2025-02-16 22:34:29 +0700

    web quynh

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector(".title");
-const text = "Happy October 20th".split("");
+const text = "Special Gift For My Akhoa".split("");
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== " ") {
     title.innerHTML += `<span>${text[index]}<span/>`;
